58b65207c14458c250a411e549b9d86ad045d948
NM77777 <89146037+NM77777@users.noreply.github.com>
2021-10-11 11:37:49 -0500

    t3

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -8,7 +8,7 @@ let five;
 function setup() {
   createCanvas(600, 600);
 
-  lonely = loadImage("assets/lonely.jpeg");
+  lonely = loadImage("assets/lonly.jpeg");
   two = loadImage("assets/two.jpeg");
   three = loadImage("assets/three.jpeg");
   alright = loadImage("assets/alright.jpeg");
@@ -31,27 +31,31 @@ function draw() {
       case 1:
        text("it's kind of lonely here", 5, 22) ;
       // put a picture here
-      image(lonely, 50, 50, 200, 200);
+       image(lonely, 50, 50, 200, 200);
       break ;
 
       case 2:
       text("Wow (sarcastic), two fingers is the best you can do?", 5, 22) ;
             // put a picture here
+            image(two, 50, 50, 200, 200);
       break ;
 
       case 3:
      text("Well its better than two.", 5, 22) ;
             // put a picture here
+            image(three, 50, 50, 200, 200);
       break ;
 
       case 4:
      text("Now we are getting some where", 5, 22) ;
             // put a picture here
+            image(alright, 50, 50, 200, 200);
       break ;
 
       case 5:
      text("Now that is a High Five", 5, 22) ;
             // put a picture here
+            image(five, 50, 50, 200, 200);
       break ;
 
   }
